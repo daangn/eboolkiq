764a4ceb848cc6583f5e6e0b0ce443b877caf1c1
isbang <forresti3979@gmail.com>
2020-10-15 23:06:48 +0900

    feat(db): add redis queue package

---
+++ internal/redis/redis.go
@@ -0,0 +1,292 @@
+// Copyright 2020 Danggeun Market Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/gomodule/redigo/redis"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/daangn/eboolkiq"
+)
+
+type redisQueue struct {
+	pool *redis.Pool
+}
+
+func NewRedisQueue(pool *redis.Pool) *redisQueue {
+	return &redisQueue{
+		pool: pool,
+	}
+}
+
+// GetQueue get queue from redis using GET command.
+func (r *redisQueue) GetQueue(ctx context.Context, queue string) (*eboolkiq.Queue, error) {
+	conn, err := r.pool.GetContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	return r.getQueue(conn, queue)
+}
+
+// PushJob push job to redis queue using LPUSH command.
+//
+// job.Id must not empty.
+func (r *redisQueue) PushJob(ctx context.Context, queue string, job *eboolkiq.Job) error {
+	conn, err := r.pool.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	q, err := r.getQueue(conn, queue)
+	if err != nil {
+		return err
+	}
+
+	return r.pushJob(conn, q.Name, job)
+}
+
+// FetchJob fetch job from redis queue using BRPOP command.
+func (r *redisQueue) FetchJob(ctx context.Context, queue string, waitTimeout time.Duration) (*eboolkiq.Job, error) {
+	conn, err := r.pool.GetContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	q, err := r.getQueue(conn, queue)
+	if err != nil {
+		return nil, err
+	}
+
+	var job *eboolkiq.Job
+	switch {
+	case waitTimeout == 0:
+		job, err = r.popJob(conn, q.Name)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		job, err = r.bpopJob(conn, q.Name, waitTimeout)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	job.Attempt += 1
+	if q.AutoFinish {
+		return job, err
+	}
+
+	if err := r.setJob(conn, q, job); err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (r *redisQueue) Succeed(ctx context.Context, jobId string) error {
+	conn, err := r.pool.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	job, _, err := r.getJob(conn, jobId)
+	if err != nil {
+		return err
+	}
+
+	if err := r.deleteJob(conn, job.Id); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *redisQueue) Failed(ctx context.Context, jobId string, errMsg string) error {
+	conn, err := r.pool.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	job, queue, err := r.getJob(conn, jobId)
+	if err != nil {
+		return err
+	}
+
+	if queue.MaxRetry == -1 && int32(job.Attempt-1) < queue.MaxRetry {
+		return r.pushJob(conn, queue.Name, job)
+	}
+	return r.failJob(conn, queue.Name, job)
+}
+
+func (r *redisQueue) getQueue(conn redis.Conn, queue string) (*eboolkiq.Queue, error) {
+	queueBytes, err := redis.Bytes(conn.Do("GET", kvQueuePrefix+queue))
+	if err != nil {
+		return nil, eboolkiq.ErrQueueNotFound
+	}
+
+	var q eboolkiq.Queue
+	if err := proto.Unmarshal(queueBytes, &q); err != nil {
+		return nil, err
+	}
+
+	return &q, nil
+}
+
+func (r *redisQueue) pushJob(conn redis.Conn, queueName string, job *eboolkiq.Job) error {
+	jobBytes, err := proto.Marshal(job)
+	if err != nil {
+		return err
+	}
+
+	_, err = redis.Int(conn.Do("LPUSH", queuePrefix+queueName, jobBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *redisQueue) popJob(conn redis.Conn, queueName string) (*eboolkiq.Job, error) {
+	jobBytes, err := redis.Bytes(conn.Do("RPOP", queuePrefix+queueName))
+	if err != nil {
+		switch err {
+		case redis.ErrNil:
+			return nil, eboolkiq.ErrEmptyQueue
+		default:
+			return nil, err
+		}
+	}
+
+	var job eboolkiq.Job
+	if err := proto.Unmarshal(jobBytes, &job); err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (r *redisQueue) bpopJob(conn redis.Conn, queueName string, timeout time.Duration) (*eboolkiq.Job, error) {
+	// brpop timeout 단위가 1초 단위기 때문에 0 이 되는 것을 방지하기 위함
+	if timeout > 0 && timeout < time.Second {
+		timeout = time.Second
+	}
+
+	bytes, err := redis.ByteSlices(conn.Do("BRPOP", queuePrefix+queueName, int64(timeout/time.Second)))
+	if err != nil {
+		switch err {
+		case redis.ErrNil:
+			return nil, eboolkiq.ErrEmptyQueue
+		default:
+			return nil, err
+		}
+	}
+
+	if len(bytes) == 0 {
+		return nil, eboolkiq.ErrEmptyQueue
+	}
+
+	var job eboolkiq.Job
+	if err := proto.Unmarshal(bytes[1], &job); err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (r *redisQueue) setJob(conn redis.Conn, q *eboolkiq.Queue, job *eboolkiq.Job) error {
+	model := JobQueue{
+		Job:     job,
+		Queue:   q,
+		StartAt: timestamppb.Now(),
+	}
+
+	modelBytes, err := proto.Marshal(&model)
+	if err != nil {
+		return err
+	}
+
+	_, err = redis.String(conn.Do("SET", kvWorkingPrefix+job.Id, modelBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *redisQueue) getJob(conn redis.Conn, jobId string) (*eboolkiq.Job, *eboolkiq.Queue, error) {
+	modelBytes, err := redis.Bytes(conn.Do("GET", kvWorkingPrefix+jobId))
+	if err != nil {
+		switch err {
+		case redis.ErrNil:
+			return nil, nil, eboolkiq.ErrJobNotFound
+		default:
+			return nil, nil, err
+		}
+	}
+
+	var model JobQueue
+	if err := proto.Unmarshal(modelBytes, &model); err != nil {
+		return nil, nil, err
+	}
+	return model.Job, model.Queue, nil
+}
+
+func (r *redisQueue) deleteJob(conn redis.Conn, jobId string) error {
+	_, err := redis.Int64(conn.Do("DEL", kvWorkingPrefix+jobId))
+	if err != nil {
+		switch err {
+		case redis.ErrNil:
+			return eboolkiq.ErrJobNotFound
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *redisQueue) failJob(conn redis.Conn, queueName string, job *eboolkiq.Job) error {
+	jobBytes, err := proto.Marshal(job)
+	if err != nil {
+		return err
+	}
+
+	_, err = redis.Int(conn.Do("LPUSH", deadQueuePrefix+queueName, jobBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *redisQueue) setQueue(conn redis.Conn, queue *eboolkiq.Queue) error {
+	queueBytes, err := proto.Marshal(queue)
+	if err != nil {
+		return err
+	}
+
+	_, err = redis.String(conn.Do("SET", kvQueuePrefix+queue.Name, queueBytes))
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/redis/redis_test.go
@@ -0,0 +1,369 @@
+// Copyright 2020 Danggeun Market Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redis
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+
+	"github.com/daangn/eboolkiq"
+)
+
+var _ eboolkiq.Queuer = (*redisQueue)(nil)
+
+func mustCleanUpRedis(t *testing.T, pool *redis.Pool) {
+	conn, err := pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("redis clean up failed:", err)
+	}
+	defer conn.Close()
+
+	if err := conn.Send("FLUSHALL"); err != nil {
+		t.Fatal("redis clean up failed:", err)
+	}
+
+	if err := conn.Flush(); err != nil {
+		t.Fatal("redis clean up failed:", err)
+	}
+}
+
+func TestRedisQueue(t *testing.T) {
+	pool := &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+		DialContext: func(ctx context.Context) (redis.Conn, error) {
+			return redis.DialContext(ctx, "tcp", "localhost:6379")
+		},
+		MaxIdle: 300,
+	}
+	defer pool.Close()
+
+	defer mustCleanUpRedis(t, pool)
+	db := NewRedisQueue(pool)
+
+	t.Run("GetQueue", testRedisQueue_GetQueue(t, db))
+	t.Run("PushJob", testRedisQueue_PushJob(t, db))
+	t.Run("FetchJob/0", testRedisQueue_FetchJob(t, db, 0))
+	t.Run("FetchJob/1ms", testRedisQueue_FetchJob(t, db, time.Second))
+	t.Run("Succeed", testRedisQueue_Succeed(t, db))
+	t.Run("Failed", testRedisQueue_Failed(t, db))
+}
+
+func testRedisQueue_GetQueue(t *testing.T, r *redisQueue) func(*testing.T) {
+	conn, err := r.pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("TestRedisQueue/GetQueue setup failed:", err)
+	}
+	defer conn.Close()
+
+	if err := r.setQueue(conn, &eboolkiq.Queue{
+		Name:       "test",
+		AutoFinish: false,
+		MaxRetry:   -1,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/GetQueue setup failed:", err)
+	}
+
+	return func(t *testing.T) {
+		defer mustCleanUpRedis(t, r.pool)
+
+		tests := []struct {
+			queue string
+			q     *eboolkiq.Queue
+			err   error
+		}{{
+			queue: "test",
+			q: &eboolkiq.Queue{
+				Name:       "test",
+				AutoFinish: false,
+				Timeout:    nil,
+				MaxRetry:   -1,
+			},
+			err: nil,
+		}, {
+			queue: "notExists",
+			q:     nil,
+			err:   eboolkiq.ErrQueueNotFound,
+		}}
+
+		for _, test := range tests {
+			q, err := r.GetQueue(context.Background(), test.queue)
+
+			if err != test.err {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"acutal  : %v\n"+
+					"params: %+v\n", test.err, err, test)
+				continue
+			}
+
+			if q.String() != test.q.String() {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual: %v\n", test.q.String(), q.String())
+			}
+		}
+	}
+}
+
+func testRedisQueue_PushJob(t *testing.T, r *redisQueue) func(*testing.T) {
+	conn, err := r.pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("TestRedisQueue/PushJob setup failed:", err)
+	}
+	defer conn.Close()
+
+	if err := r.setQueue(conn, &eboolkiq.Queue{
+		Name: "test",
+	}); err != nil {
+		t.Fatal("TestRedisQueue/PushJob setup failed:", err)
+	}
+
+	return func(t *testing.T) {
+		defer mustCleanUpRedis(t, r.pool)
+
+		tests := []struct {
+			queue string
+			job   *eboolkiq.Job
+			err   error
+		}{{
+			queue: "test",
+			job: &eboolkiq.Job{
+				Id:          "testjobid",
+				Description: "foo bar",
+			},
+			err: nil,
+		}, {
+			queue: "not exists",
+			job:   nil,
+			err:   eboolkiq.ErrQueueNotFound,
+		}}
+
+		for _, test := range tests {
+			err := r.PushJob(context.Background(), test.queue, test.job)
+			if err != test.err {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual:   %v\n", test.err, err)
+			}
+		}
+	}
+}
+
+func testRedisQueue_FetchJob(t *testing.T, r *redisQueue, timeout time.Duration) func(t *testing.T) {
+	conn, err := r.pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("TestRedisQueue/FetchJob setup failed:", err)
+	}
+	defer conn.Close()
+
+	if err := r.setQueue(conn, &eboolkiq.Queue{
+		Name: "test",
+	}); err != nil {
+		t.Fatal("TestRedisQueue/FetchJob setup failed:", err)
+	}
+
+	if err := r.setQueue(conn, &eboolkiq.Queue{
+		Name:       "autoFinish",
+		AutoFinish: true,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/FetchJob setup failed:", err)
+	}
+
+	if err := r.pushJob(conn, "test", &eboolkiq.Job{
+		Id:          "testjobid",
+		Description: "foo bar",
+		Attempt:     0,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/FetchJob setup failed:", err)
+	}
+
+	if err := r.pushJob(conn, "autoFinish", &eboolkiq.Job{
+		Id:          "autoFinishJobId",
+		Description: "foo bar",
+		Attempt:     0,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/FetchJob setup failed:", err)
+	}
+
+	return func(t *testing.T) {
+		defer mustCleanUpRedis(t, r.pool)
+
+		tests := []struct {
+			queue string
+			job   *eboolkiq.Job
+			err   error
+		}{{
+			queue: "test",
+			job: &eboolkiq.Job{
+				Id:          "testjobid",
+				Description: "foo bar",
+				Attempt:     1,
+			},
+			err: nil,
+		}, {
+			queue: "autoFinish",
+			job: &eboolkiq.Job{
+				Id:          "autoFinishJobId",
+				Description: "foo bar",
+				Attempt:     1,
+			},
+			err: nil,
+		}, {
+			queue: "test",
+			job:   nil,
+			err:   eboolkiq.ErrEmptyQueue,
+		}}
+
+		for _, test := range tests {
+			job, err := r.FetchJob(context.Background(), test.queue, timeout)
+
+			if err == context.DeadlineExceeded {
+				continue
+			}
+
+			if err != test.err {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual:   %v\n"+
+					"with:     %+v\n", test.err, err, test)
+				t.Log(job.String())
+				continue
+			}
+
+			if err != nil {
+				continue
+			}
+
+			if test.job.String() != job.String() {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual:   %v\n"+
+					"with:     %+v\n", test.job.String(), job.String(), test)
+			}
+		}
+	}
+}
+
+func testRedisQueue_Succeed(t *testing.T, r *redisQueue) func(*testing.T) {
+	conn, err := r.pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("TestRedisQueue/Succeed setup failed:", err)
+	}
+	defer conn.Close()
+
+	job := &eboolkiq.Job{
+		Id:          "testJobId",
+		Description: "foo bar",
+		Attempt:     1,
+	}
+
+	queue := &eboolkiq.Queue{
+		Name:       "test",
+		AutoFinish: false,
+		Timeout:    nil,
+		MaxRetry:   0,
+	}
+
+	if err := r.setJob(conn, queue, job); err != nil {
+		t.Fatal(err)
+	}
+
+	return func(t *testing.T) {
+		defer mustCleanUpRedis(t, r.pool)
+
+		tests := []struct {
+			jobId string
+			err   error
+		}{{
+			jobId: "testJobId",
+			err:   nil,
+		}, {
+			jobId: "unknownId",
+			err:   eboolkiq.ErrJobNotFound,
+		}}
+
+		for _, test := range tests {
+			if err := r.Succeed(context.Background(), test.jobId); err != test.err {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual:   %v\n"+
+					"with:     %+v\n", test.err, err, test)
+				t.Log(job.String())
+			}
+		}
+	}
+}
+
+func testRedisQueue_Failed(t *testing.T, r *redisQueue) func(*testing.T) {
+	conn, err := r.pool.GetContext(context.Background())
+	if err != nil {
+		t.Fatal("TestRedisQueue/Failed setup failed:", err)
+	}
+	defer conn.Close()
+
+	if err := r.setJob(conn, &eboolkiq.Queue{
+		Name:     "test1",
+		MaxRetry: 0,
+	}, &eboolkiq.Job{
+		Id:          "retryJob",
+		Description: "foo bar",
+		Attempt:     1,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/Failed setup failed:", err)
+	}
+
+	if err := r.setJob(conn, &eboolkiq.Queue{
+		Name:     "test2",
+		MaxRetry: 1,
+	}, &eboolkiq.Job{
+		Id:          "deadJob",
+		Description: "foo bar test",
+		Attempt:     1,
+	}); err != nil {
+		t.Fatal("TestRedisQueue/Failed setup failed:", err)
+	}
+
+	return func(t *testing.T) {
+		defer mustCleanUpRedis(t, r.pool)
+
+		tests := []struct {
+			jobId string
+			err   error
+		}{{
+			jobId: "retryJob",
+			err:   nil,
+		}, {
+			jobId: "deadJob",
+			err:   nil,
+		}, {
+			jobId: "unknownJob",
+			err:   eboolkiq.ErrJobNotFound,
+		}}
+
+		for _, test := range tests {
+			if err := r.Failed(context.Background(), test.jobId, "just failed"); err != test.err {
+				t.Errorf("test failed\n"+
+					"expected: %v\n"+
+					"actual:   %v\n"+
+					"with:     %+v\n", test.err, err, test)
+			}
+		}
+	}
+}
