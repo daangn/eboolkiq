a4a3fa2790d44cd1f86880da1bae5e62bb952b3a
isbang <forresti3979@gmail.com>
2020-11-07 23:43:56 +0900

    refactor(proto): remove proto dir

--- script/genproto.sh
+++ script/genproto.sh
@@ -1,15 +1,17 @@
-#!/bin/zsh
+#!/bin/sh
 
-for f in **/*.proto
+for f in $(find . -name "*.proto" -not -path "*internal*")
+do
+    protoc -I . -I /usr/local/include \
+        --go_out=plugins=grpc:pb \
+        --go_opt=paths=source_relative \
+        "${f}"
+done
+
+for f in $(find internal -name "*.proto")
 do
     protoc -I . -I /usr/local/include \
         --go_out=plugins=grpc:. \
         --go_opt=paths=source_relative \
         "${f}"
-done
-
-for f in **/*.pb.go
-do
-  mkdir -p "$(dirname "$f")"
-  mv "${f}" "${f//proto/pb}"
 done
