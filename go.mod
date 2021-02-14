c51b561ad9d696e6bdd0d5aff6b7db18f96c1ac8
isbang <forresti3979@gmail.com>
2021-02-14 23:25:58 +0900

    feat(proto): add protobuf files

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/daangn/eboolkiq
 
 go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.35.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
+	google.golang.org/protobuf v1.25.0
+)
---
+++ script/genproto.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# install dependencies
+go mod tidy && go install \
+  google.golang.org/protobuf/cmd/protoc-gen-go \
+  google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+
+# remove old pb files
+rm -rf pb/*
+
+
+# generate protobuf file
+for f in $(find ./proto -name "*.proto")
+do
+  protoc -I ./proto \
+    --go_out pb \
+    --go_opt paths=source_relative \
+    --go-grpc_out pb \
+    --go-grpc_opt paths=source_relative \
+    "${f}"
+done
+
+
+# move and delete directories
+mv pb/daangn/eboolkiq/* pb/ \
+&& find ./pb -type d -empty -delete
