054d6d6dc84fdb2189bc0183f4eee8adb9927c37
isbang <forresti3979@gmail.com>
2021-02-17 03:43:31 +0900

    docs: update license year

--- tools.go
+++ tools.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Danggeun Market Inc.
+// Copyright 2021 Danggeun Market Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
